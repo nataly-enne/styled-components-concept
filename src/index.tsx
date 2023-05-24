34ec9ada0beb698dd0cafb079b8c1328f89e3684
nataly-enne <ennenataly@gmail.com>
2023-05-23 21:09:58 -0300

    finalizando configurações e instalando styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styled from 'styled-components';
+
+const Body = styled.body`
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Body>
+      <App />
+    </Body>
+  </StrictMode>
 );
-reportWebVitals();
